5d9b1a023329639d4565b088657d8b6c310fa2b5
Oleksandr Oleksyuk <oleksandr.oleksyuk97@icloud.com>
2022-12-21 19:52:47 +0100

    fixed bug

--- src/js/app.js
+++ src/js/app.js
@@ -1,59 +1,16 @@
 "use strict";
-
-// import * as module from "./module";
 import { API_URL_VIDEO, API_KEY } from "./config";
 import { getJSON } from "./helpers";
-import view from "./view/view";
-// console.log(module.state);
-const state = {
-  items: [],
-};
-
-const createObject = function (data) {
-  return data.items.map((element) => {
-    // console.log(element.channelThumbnail); // BUGz
-    ///// ===== creo quanti giorni fa è stato publicato il video
-    const publishedAt =
-      (Date.now() - Date.parse(new Date(element.snippet.publishedAt))) /
-      (1000 * 60 * 60);
-    const dayAt = Math.floor(publishedAt / 24);
-    const hourAt = Math.floor(publishedAt);
-    const strTimePassated =
-      dayAt >= 1
-        ? `${dayAt} giorn${dayAt === 1 ? "o" : "i"} fa`
-        : `${hourAt} ore fa`;
-
-    //// ====== RITORNO IL MIO OGGETTO FINALE
-    return {
-      id: element.id,
-      imageUrl: element.snippet.thumbnails.high.url,
-      title: element.snippet.title,
-      channelTitle: element.snippet.channelTitle,
-      publishedAt: strTimePassated,
-      viewCount: element.statistics.viewCount,
-    };
-  });
-  //   console.log(data.items);
-};
+import view from "./view/view.js";
 
 const loadData = async function () {
   try {
     const data = await getJSON(API_URL_VIDEO);
-    // aggiungo le icone al mio oggetto data
-
-    state.items = [...createObject(data)];
-    const arr = await data.items.map(async (el, i) => {
-      const azzz = await getChannelIcons(el);
-      return (data.items[i].abcd = azzz);
-    });
-
-    // RENDERIZZO I MIEI ELEMENTI
-    state.items.forEach((data) => view.render(data));
+    data.items.forEach((element) => getChannelIcons(element));
   } catch (error) {
     console.error(error);
   }
 };
-console.log(state);
 
 const getChannelIcons = async function (dataVideo) {
   try {
@@ -66,10 +23,12 @@ const getChannelIcons = async function (dataVideo) {
         })
     );
     const data = await res.json();
-    return data.items[0].snippet.thumbnails.default.url;
+    // AGGIUNGO LE ICONE
+    dataVideo.abcd = data.items[0].snippet.thumbnails.default.url;
+    // RENDERIZZO I MIEI ELEMENTI
+    view.render(dataVideo);
   } catch (error) {
     console.error(error);
   }
 };
-
 loadData();
--- src/js/view/view.js
+++ src/js/view/view.js
@@ -1,38 +1,46 @@
 class View {
   #parentElement = document.getElementById("container");
-  #data;
+  data;
   render(data) {
-    this.#data = data;
+    this.data = data;
     const markup = this._generateMarkup();
 
     this.#parentElement.insertAdjacentHTML("afterbegin", markup);
   }
 
   _generateMarkup() {
-    console.log(this.#data);
-    console.log(this.#data.abcd);
-    console.log(this.#data);
-    return `<div id="${this.#data.id}">
+    // console.log(this.data);
+    // console.log(this.data.abcd);
+    // console.log(this.data);
+    const publishedAt =
+      (Date.now() - Date.parse(new Date(this.data.snippet.publishedAt))) /
+      (1000 * 60 * 60);
+    const dayAt = Math.floor(publishedAt / 24);
+    const hourAt = Math.floor(publishedAt);
+    const strTimePassated =
+      dayAt >= 1
+        ? `${dayAt} giorn${dayAt === 1 ? "o" : "i"} fa`
+        : `${hourAt} ore fa`;
+    console.log();
+    return `<div id="${this.data.id}">
               <img class="rounded-xl mb-3" src="${
-                this.#data.imageUrl
+                this.data.snippet.thumbnails.high.url
               }" alt="" />
               <div class="grid grid-cols-5 ">
                 <div class="flex justify-center">
                   <div class="h-8 w-8 rounded-full bg-green-500">
-                  <img src="${this.#data.abcd}" alt="" />
+                  <img src="${this.data.abcd}" alt="" />
                   </div>
                 </div>
                 <div class="col-span-4">
                   <h2 class="text-sm mb-3">
-                    ${this.#data.title}
+                    ${this.data.snippet.title}
                   </h2>
-                  <p class="text-xs text-neutral-400">${
-                    this.#data.channelTitle
-                  }</p>
+                  <p class="text-xs text-neutral-400">${""}</p>
                   <p class="text-xs text-neutral-400">
-                    ${this.#data.viewCount} visualizzazioni - <span>${
-      this.#data.publishedAt
-    }</span>
+                    ${
+                      this.data.statistics.viewCount
+                    } visualizzazioni - <span>${22}</span>
                   </p>
                 </div>
               </div>
